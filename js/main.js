7bfc12227f197c48951253104b7986a701a5995b
jay1675 <gnlrud3832@gmail.com>
2024-11-10 21:45:56 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,41 +60,4 @@ window.addEventListener("scroll", function(){
 
 })
 
-// Favorite Slide
-const slides = document.querySelector(".slides");
-const slide = document.querySelectorAll(".slides li");
-const slideCount = slide.length;
-
-let currentIdx = 0;
-
-let slideWidth = 300;
-let slideMargin = 30;
-
-slides.style.width = (slideWidth + slideMargin) * slideCount - slideMargin + 'px';
-
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next")
-
-function moveSlide(num){
-    // slide 가 움직일때 slide index가 1이면 -330px 2면 -660px
-    slides.style.left = -num * 330 +'px';
-    currentIdx = num;
-}
-
-
-nextBtn.addEventListener('click', function(){
-    if(currentIdx < slideCount-3){
-        moveSlide(currentIdx + 1);
-    } else {
-        moveSlide(0);
-    }
-})
-
-prevBtn.addEventListener('click', function(){
-    if(currentIdx > 0){
-        moveSlide(currentIdx - 1);
-    } else {
-        moveSlide(slideCount - 3);
-    }
-})
 
